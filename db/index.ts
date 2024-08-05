97bc5ffe09af22414f82e6440f4a8d60b99e58e8
Philipp Hermes <philipp.hermes@cec.valantic.com>
2024-08-05 14:28:26 +0200

    fix path to db

--- db/index.ts
+++ db/index.ts
@@ -3,7 +3,7 @@ import Database from "better-sqlite3";
 import {migrate} from "drizzle-orm/better-sqlite3/migrator";
 import * as schema from "./schema";
 
-const sqlite = new Database("database.db");
+const sqlite = new Database("./db/database.db");
 export const db = drizzle(sqlite, {schema});
 
 migrate(db, {migrationsFolder: "./db/migrations"});
